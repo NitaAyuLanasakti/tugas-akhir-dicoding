628af3bdaa8d6685d97cee7c49b13e2f1e8df650
NitaAyuLanasakti <101516415+NitaAyuLanasakti@users.noreply.github.com>
2022-03-15 20:54:27 +0800

    Add files via upload

---
+++ routes.js
@@ -0,0 +1,45 @@
+const { addBookHandler, getAllBooksHandler, getBookByIdHandler, editBookByIdHandler, deleteBookByIdHandler } = require('./handler');
+
+const routes = [{
+        method: 'POST',
+        path: '/books',
+        handler: addBookHandler,
+        options: {
+            cors: {
+                origin: ['*'],
+            },
+        },
+    },
+    {
+        method: 'GET',
+        path: '/books',
+        handler: getAllBooksHandler,
+        options: {
+            cors: {
+                origin: ['*'],
+            },
+        },
+    },
+    {
+        method: 'GET',
+        path: '/books/{id}',
+        handler: getBookByIdHandler,
+        options: {
+            cors: {
+                origin: ['*'],
+            },
+        },
+    },
+    {
+        method: 'PUT',
+        path: '/books/{id}',
+        handler: editBookByIdHandler,
+    },
+    {
+        method: 'DELETE',
+        path: '/books/{id}',
+        handler: deleteBookByIdHandler,
+    },
+
+];
+module.exports = routes;
